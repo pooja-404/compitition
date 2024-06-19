92167380c3c422d4a63aac7a7131cd4bd2b35feb
pooja-404 <pk3924792@gmail.com>
2024-06-19 14:21:41 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,26 @@
+function openNav() {
+    document.getElementById("navbar").classList.toggle("end-0");
+    document.body.classList.toggle("overflow_hidden");
+    document.getElementById("menubtn-icon").classList.toggle("cross")
+
+}
+window.addEventListener('scroll', function () {
+    let backtotop = document.getElementById("backtotop")
+    if (window.scrollY > 300) {
+        backtotop.style.display = 'block'
+    }
+    else {
+        backtotop.style.display = 'none'
+    }
+})
+
+setTimeout(() => {
+    document.getElementById("preloader").classList.add("d-none");
+    document.getElementById("preloader").classList.add("pointer_event_none")
+    document.body.classList.remove("overflow-hidden")
+}
+    , 3000);
+let s = document.getElementById("name")
+const y = new Date()
+const year = y.getFullYear();
+s.innerHTML = "© " + year + " cloopp"
